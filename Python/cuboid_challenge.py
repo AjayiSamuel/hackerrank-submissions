e500e685e6b4c41409dc6f833e5d33d6211114b0
SAMUEL AJAYI <sajayi@appzonegroup.com>
2020-01-26 09:25:42 +0100

    runner up score script added

--- Python/cuboid_challenge.py
+++ Python/cuboid_challenge.py
@@ -1,3 +1,16 @@
+def cube_value_check(x,y,z,n):
+    the_list = [[i, j, k] for i in range(x+1) for j in range(y + 1) for k in range(z + 1) if((i + j + k) != n)]
+    return the_list
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    print(cube_value_check(x,y,z,n))
+
+
+
 # x = int ( raw_input())
 # y = int ( raw_input())
 # n = int ( raw_input())
@@ -22,8 +35,8 @@
 
 # x, y, z, n = 2, 2, 2, 2
 
-def cube_check(x,y,z,n):
-    the_list = [[i, j, k] for i in range(x+1) for j in range(y + 1) for k in range(z + 1) if((i + j + k) != n)]
-    return the_list
-
-print(cube_check(2,2,2,2))
+# def cube_check(x,y,z,n):
+#     the_list = [[i, j, k] for i in range(x+1) for j in range(y + 1) for k in range(z + 1) if((i + j + k) != n)]
+#     return the_list
+#
+# print(cube_check(1,1,1,2))
---
+++ Python/second_highest_number.py
@@ -0,0 +1,15 @@
+def runner_up_score(arr):
+    numbers = list(arr)
+    max_num = max(numbers)
+    numbers = [x for x in numbers if x != max_num]
+    new_max = max(numbers)
+    return new_max
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    print(runner_up_score(arr))
+
+
+
+
